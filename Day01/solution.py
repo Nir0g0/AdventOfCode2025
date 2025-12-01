1be383ebbfeee8f9bec6d3c21e5340d4af16632f
Laptop <logischermeister@gmail.com>
2025-12-01 17:41:45 +0100

    day 1

---
+++ Day01/solution.py
@@ -0,0 +1,38 @@
+counterZero = 0
+dial = 50
+
+# Star 1
+with open("input.txt") as file:
+    for line in file:
+        direction = line[:1]
+        numberOfTurns = int(line[1:])
+
+        if direction == 'L':
+            dial = (dial - numberOfTurns) % 100
+        else:
+            dial = (dial + numberOfTurns) % 100
+
+        if dial == 0: counterZero += 1
+
+print(counterZero)
+
+# Star 2
+
+counterZero = 0
+dial = 50
+
+with open("input.txt") as file:
+    for line in file:
+        direction = line[:1]
+        numberOfTurns = int(line[1:])
+        print(f"dial: {dial}, {numberOfTurns}({direction})")
+        print("zero: ", counterZero)
+        for i in range(0, numberOfTurns, 1):
+            if direction == 'L':
+                dial = (dial - 1) % 100
+            else:
+                dial = (dial + 1) % 100
+            
+            if dial == 0: counterZero += 1
+
+print(counterZero)
